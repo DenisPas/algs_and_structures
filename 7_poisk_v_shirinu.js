72a3c145db7c6e2fe0116f936906b794c00d922b
DenisPas <denis.pas2012@yandex.ru>
2022-03-14 12:33:13 +0300

    Update 7_poisk_v_shirinu.js

--- 7_poisk_v_shirinu.js
+++ 7_poisk_v_shirinu.js
@@ -26,3 +26,34 @@ function breadthSearch(graph, start, end) {
 }
 
 console.log(breadthSearch(graph, 'a', 'e'))
+
+function bfs(adj, s, t) {
+	// adj - смежный список
+	// s - начальная вершина
+	// t - пункт назначения
+
+	// инициализируем очередь
+	let queue = []
+	// добавляем s в очередь
+	queue.push(s)
+	// помечаем s как посещенную вершину во избежание повторного добавления в очередь
+	s.visited = true
+	while(queue.length > 0) {
+		// удаляем первый (верхний) элемент из очереди
+		let v = queue.shift()
+		// abj[v] - соседи v
+		for(let neighbor of adj[v]) {
+			// если сосед не посещался
+			if(!neighbor.visited) {
+				// добавляем его в очередь
+				queue.push(neighbor)
+				// помечаем вершину как посещенную
+				neighbor.visited = true
+				// если сосед является пунктом назначения, мы победили
+				if(neighbor === t) return true
+			}
+		} 
+	}
+	// если t не обнаружено, значит пункта назначения достичь невозможно
+	return false
+}
